b2db8d3230af274fe3bf2c21e1d678b608bcd79d
Radik Yamaleev <dev.shazlogic@gmail.com>
2025-10-22 00:08:06 +0300

    add: Coin + destroy

---
+++ Assets/PixelCrew/Components/Coins/DestroyObjectComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Components.Coins
+{
+    public class DestroyObjectComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectToDestroy;
+        public void DestroyObject()
+        {
+            Destroy(_objectToDestroy);
+        }
+    }
+}
