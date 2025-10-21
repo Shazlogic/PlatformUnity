9c3dd52c1dc1ed8583ccdaf4d72240354f696319
Radik Yamaleev <dev.shazlogic@gmail.com>
2025-10-21 22:31:49 +0300

    add: Reset scene

---
+++ Assets/PixelCrew/Components/ReloadEventComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PixelCrew
+{
+    public class ReloadEventComponent : MonoBehaviour
+    {
+        public void Reload()
+        {
+            var scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
