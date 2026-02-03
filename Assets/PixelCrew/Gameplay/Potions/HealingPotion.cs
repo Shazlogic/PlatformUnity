aaf67ee8f0b60336368910ad3f7e09e49a99c100
Radik Yamaleev <35137419+Shazlogic@users.noreply.github.com>
2026-02-03 14:16:52 +0300

    feature/potions (#18)

---
+++ Assets/PixelCrew/Gameplay/Potions/HealingPotion.cs
@@ -0,0 +1,40 @@
+﻿using Assets.PixelCrew.Gameplay.Potions;
+using PixelCrew.Core.Components;
+using UnityEngine;
+
+public class HealingPotion : MonoBehaviour
+{
+    [SerializeField] private int _heal;
+    [SerializeField] private SpriteAnimationPick _pickupAnimation;
+
+    public void ApplyHeal(GameObject target)
+    {
+        var healthComponent = target.GetComponent<HealthComponent>();
+
+        if (healthComponent != null)
+        {
+            healthComponent.Heal(_heal);
+            PlayPickupAnimation();
+        }
+    }
+
+    private void PlayPickupAnimation()
+    {
+        GetComponent<Collider2D>().enabled = false;
+
+        if (_pickupAnimation != null)
+        {
+            _pickupAnimation.OnComplete += OnAnimationComplete;
+            _pickupAnimation.Play();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnAnimationComplete()
+    {
+        Destroy(gameObject);
+    }
+}
