a31e61c823460202b327be78a9b1c14793caf1bb
Radik Yamaleev <dev.shazlogic@gmail.com>
2026-02-03 14:16:02 +0300

    add: Heal potion

--- Assets/PixelCrew/Core/Components/HealthComponent.cs
+++ Assets/PixelCrew/Core/Components/HealthComponent.cs
@@ -6,7 +6,9 @@ namespace PixelCrew.Core.Components
     public class HealthComponent : MonoBehaviour
     {
         [SerializeField] private int _health;
+        [SerializeField] private int _maxHealth;
         [SerializeField] private UnityEvent _onDamage;
+        [SerializeField] private UnityEvent _onHeal;
         [SerializeField] private UnityEvent _onDie;
 
         public void ApplyDamage(int damageValue)
@@ -14,10 +16,22 @@ namespace PixelCrew.Core.Components
             _health -= damageValue;
             _onDamage?.Invoke();
 
-            if(_health <= 0)
+            if (_health <= 0)
             {
                 _onDie?.Invoke();
             }
+        }
+
+        public void Heal(int healValue)
+        {
+            _health += healValue;
+
+            if (_maxHealth > 0 && _health > _maxHealth)
+            {
+                _health = _maxHealth;
+            }
+
+            _onHeal?.Invoke();
         }
     }
 }
---
+++ Assets/PixelCrew/Gameplay/Potions/SpriteAnimationPick.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.PixelCrew.Gameplay.Potions
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+
+    public class SpriteAnimationPick : MonoBehaviour
+    {
+        [SerializeField] private int _frameRate;
+        [SerializeField] private Sprite[] _sprites;
+
+        public UnityAction OnComplete;
+        private SpriteRenderer _renderer;
+        private float _secondPerFrame;
+        private int _currentSpriteIndex;
+        private float _nextFrameTime;
+        private bool _isPlaying = true;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+            _secondPerFrame = 1f / _frameRate;
+            _nextFrameTime = Time.time + _secondPerFrame;
+        }
+
+        public void Play()
+        {
+            _isPlaying = true;
+            _currentSpriteIndex = 0;
+            _nextFrameTime = Time.time + _secondPerFrame;
+        }
+
+        private void Update()
+        {
+            if (!_isPlaying || _nextFrameTime > Time.time) return;
+
+            if (_currentSpriteIndex >= _sprites.Length)
+            {
+                _isPlaying = false;
+                OnComplete?.Invoke();
+                return;
+            }
+
+            _renderer.sprite = _sprites[_currentSpriteIndex];
+            _nextFrameTime += _secondPerFrame;
+            _currentSpriteIndex++;
+        }
+    }
+}
